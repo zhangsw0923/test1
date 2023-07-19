e0ca8291ef82ca9b4e5d6a75c3901ded17356450
张威 <zhangwei@ZhangVMBP.local>
2023-07-19 17:53:25 +0800

    Initial commit

---
+++ p1.py
@@ -0,0 +1,16 @@
+age = 18
+name = 'TOM'
+weight = 75.5
+stu_id=1
+
+# 格式化符号
+print("我的名字叫%s,我今年%d岁，体重%.2f，学号是%04d " % (name,age,weight,stu_id))
+
+# f格式化方式
+print (f'我今年{age}岁，我的名字叫{name}')
+
+
+# \n 换行
+# \t 制表符
+# print结束符
+# print('内容',end="")
